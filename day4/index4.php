8990078a4d2d95151c7cbce61da3e9517dda2342
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-02 16:20:00 +0700

    hàm sort() sắp xếp mảng theo thứ tự tăng dần

---
+++ day4/index4.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$arr1 = array(24,7,28,100,1,7,9);
+/**
+ * Sắp xếp lại mảng theo thứ tự tăng dần
+ * sử dụng hàm sort();
+ */
+echo "<pre>";
+print_r($arr1);
+echo "</pre>";
+sort($arr1);
+echo "<pre>";
+print_r($arr1);
+echo "</pre>";
+?>
+</body>
+</html>
