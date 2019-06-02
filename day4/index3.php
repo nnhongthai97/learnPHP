354710ecfddcb76afe80149dbc30a95912200d40
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-02 16:14:32 +0700

    hàm count để đếm số phần tử của mảng

---
+++ day4/index3.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+$arr1 = array(1,2,3,7,9,11);
+// Đếm số phần tử của mảng này
+// sử dụng hàm count để đếm
+echo count($arr1);
+?>
+</body>
+</html>
