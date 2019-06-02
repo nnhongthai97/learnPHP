50caa3d3d2f1d2440bb5a9db4e87aaaee4f8ef82
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-02 16:04:12 +0700

    câu lệnh break trong php

---
+++ day4/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+    for( $i = 1; $i <=10 ; $i++){
+        if( $i==5){
+            break;
+        }
+
+    }
+    echo"<br>".$i;
+?>
+</body>
+</html>
