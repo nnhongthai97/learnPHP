2f078f6e8968ba5e2645bec69274e8fea94de9d7
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-16 18:49:26 +0700

    opp php

---
+++ day5/OPP/opp2.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Yêu cầu : Xây dựng class tên là Database
+ * có 1 thuộc tính là connection để lưu trữ kết nội đến CSDL
+ * và có 4 thuộc tính : ip database ( localhost ) , user db , pass db , tên CSDL
+ * Class này có 1 method khởi tạo là __construct() làm nhiệm vụ kết nội đến CSDL
+ * constructor này sẽ có 4 tham số dùng để kết nối đến CSDL truyền vào
+ * và trong method này sẽ khởi tạo kết nối CSDL và gán kết nối cho thuộc tính connection
+ *
+ */
+class Database{
+    public $host = "localhost";
+    public $user = "root";
+    public $password = "";
+    public $database = "demo3";
+    public $connection;
+    /*
+     * Phương thức khởi tạo
+     */
+    public function __construct($ipDatabase, $userDatabase, $passDatabase, $databaseName) {
+        $this->ipDatabase = $ipDatabase;
+        $this->userDatabase = $userDatabase;
+        $this->passDatabase = $passDatabase;
+        $this->databaseName = $databaseName;
+        $this->connection = new mysqli($ipDatabase, $userDatabase, $passDatabase, $databaseName);
+    }
+
+    public function disconnect()
+    {
+        mysqli_close($this->connection);
+    }
+}
+
+
+?>
